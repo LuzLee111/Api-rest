6cca67d88dcd1fa145d0fa55a900e8a7a96fa588
LuzLee111 <maryluz2607133@gmail.com>
2024-09-29 21:53:31 -0300

    Delete src directory

--- src/main/java/com/example/inicial1/config/CustomRevisionListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.inicial1.config;
-
-public class CustomRevisionListener implements ReevisionListener{
-    public void newRevision (Object revisionEntity) {final Revision revision =(Revision) revisionEntity;}
-}
